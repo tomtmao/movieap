246ab1bc1c3d8c870fb80b342020f938761485ba
tomtmao <tomtmao@outlook.com>
2019-11-14 19:48:42 +0800

    进行了eject，配置了装饰器语法。并重新配置了antd-mobile

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'antd-mobile/dist/antd-mobile.css';
+import "lib-flexible"
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
